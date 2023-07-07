3cd9192597086c455bb18b7a6ab65f67480c7fc1
Pathorn Tengkiattrakul <pathorn.teng@gmail.com>
2023-07-07 11:14:18 +0700

    add from

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const main = async () => {
     gasPrice: gasPrice,
     gasLimit: gasLimit,
     nonce: nonce,
+    from: signer.address,
   };
   // Send a transaction
   const submittedTx = await signer.sendTransaction(tx);
